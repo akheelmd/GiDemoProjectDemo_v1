ef5dcd4fbfa30ac2ffb2f3a5d7774062c2272aec
PC <PC@PC-PC>
2020-04-16 16:31:45 +0530

    fist commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test{
+	public static void main(String args[]) {
+		String s="yes";
+		if(s =="yes")
+			System.out.println("Yest, testing working fine");
+		else
+			System.out.println("Sorry testing is not working fine");
+	}
+	
+}
